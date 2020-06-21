2382a48d563a02324d2b8f771ee5e08e43affb05
316589 <1606326@tcs.com>
2020-06-21 12:03:52 +0530

    1st

---
+++ sample.js
@@ -0,0 +1,362 @@
+var data = [
+	{
+		_id: {
+			storeid: '5ec54127af24f138c304c6e7',
+			associateid: '5ec5846e17e753712d87eb46',
+			slotid: '5ec584a817e753712d87eb54',
+			slotstarttime: '10:00',
+			custLength: 0,
+			storename: 'Test Store',
+			associatename: 'Associate_2'
+		},
+		slotsAvailable: 1
+	},
+	{
+		_id: {
+			storeid: '5ec54127af24f138c304c6e7',
+			associateid: '5ec5844b17e753712d87eb43',
+			slotid: '5ec584a117e753712d87eb49',
+			slotstarttime: '10:00',
+			custLength: 0,
+			storename: 'Test Store',
+			associatename: 'Associate_1'
+		},
+		slotsAvailable: 1
+	},
+	{
+		_id: {
+			storeid: '5ec54127af24f138c304c6e7',
+			associateid: '5ec63793345c844e2415d658',
+			slotid: '5ec637a2345c844e2415d65b',
+			slotstarttime: '10:00',
+			custLength: 1,
+			storename: 'Test Store',
+			associatename: 'Associate_3'
+		},
+		slotsAvailable: 0
+	},
+	{
+		_id: {
+			storeid: '5ec54127af24f138c304c6e7',
+			associateid: '5ec5844b17e753712d87eb43',
+			slotid: '5ec584a117e753712d87eb4a',
+			slotstarttime: '11:00',
+			custLength: 0,
+			storename: 'Test Store',
+			associatename: 'Associate_1'
+		},
+		slotsAvailable: 1
+	},
+	{
+		_id: {
+			storeid: '5ec54127af24f138c304c6e7',
+			associateid: '5ec5846e17e753712d87eb46',
+			slotid: '5ec584a817e753712d87eb55',
+			slotstarttime: '11:00',
+			custLength: 0,
+			storename: 'Test Store',
+			associatename: 'Associate_2'
+		},
+		slotsAvailable: 1
+	},
+	{
+		_id: {
+			storeid: '5ec54127af24f138c304c6e7',
+			associateid: '5ec63793345c844e2415d658',
+			slotid: '5ec637a2345c844e2415d65c',
+			slotstarttime: '11:00',
+			custLength: 0,
+			storename: 'Test Store',
+			associatename: 'Associate_3'
+		},
+		slotsAvailable: 1
+	},
+	{
+		_id: {
+			storeid: '5ec54127af24f138c304c6e7',
+			associateid: '5ec5846e17e753712d87eb46',
+			slotid: '5ec584a817e753712d87eb56',
+			slotstarttime: '12:00',
+			custLength: 0,
+			storename: 'Test Store',
+			associatename: 'Associate_2'
+		},
+		slotsAvailable: 1
+	},
+	{
+		_id: {
+			storeid: '5ec54127af24f138c304c6e7',
+			associateid: '5ec5844b17e753712d87eb43',
+			slotid: '5ec584a117e753712d87eb4b',
+			slotstarttime: '12:00',
+			custLength: 0,
+			storename: 'Test Store',
+			associatename: 'Associate_1'
+		},
+		slotsAvailable: 1
+	},
+	{
+		_id: {
+			storeid: '5ec54127af24f138c304c6e7',
+			associateid: '5ec63793345c844e2415d658',
+			slotid: '5ec637a2345c844e2415d65d',
+			slotstarttime: '12:00',
+			custLength: 0,
+			storename: 'Test Store',
+			associatename: 'Associate_3'
+		},
+		slotsAvailable: 1
+	},
+	{
+		_id: {
+			storeid: '5ec54127af24f138c304c6e7',
+			associateid: '5ec5844b17e753712d87eb43',
+			slotid: '5ec584a117e753712d87eb4c',
+			slotstarttime: '13:00',
+			custLength: 0,
+			storename: 'Test Store',
+			associatename: 'Associate_1'
+		},
+		slotsAvailable: 1
+	},
+	{
+		_id: {
+			storeid: '5ec54127af24f138c304c6e7',
+			associateid: '5ec5846e17e753712d87eb46',
+			slotid: '5ec584a817e753712d87eb57',
+			slotstarttime: '13:00',
+			custLength: 0,
+			storename: 'Test Store',
+			associatename: 'Associate_2'
+		},
+		slotsAvailable: 1
+	},
+	{
+		_id: {
+			storeid: '5ec54127af24f138c304c6e7',
+			associateid: '5ec63793345c844e2415d658',
+			slotid: '5ec637a2345c844e2415d65e',
+			slotstarttime: '13:00',
+			custLength: 0,
+			storename: 'Test Store',
+			associatename: 'Associate_3'
+		},
+		slotsAvailable: 1
+	},
+	{
+		_id: {
+			storeid: '5ec54127af24f138c304c6e7',
+			associateid: '5ec5844b17e753712d87eb43',
+			slotid: '5ec584a117e753712d87eb4d',
+			slotstarttime: '14:00',
+			custLength: 0,
+			storename: 'Test Store',
+			associatename: 'Associate_1'
+		},
+		slotsAvailable: 1
+	},
+	{
+		_id: {
+			storeid: '5ec54127af24f138c304c6e7',
+			associateid: '5ec5846e17e753712d87eb46',
+			slotid: '5ec584a817e753712d87eb58',
+			slotstarttime: '14:00',
+			custLength: 0,
+			storename: 'Test Store',
+			associatename: 'Associate_2'
+		},
+		slotsAvailable: 1
+	},
+	{
+		_id: {
+			storeid: '5ec54127af24f138c304c6e7',
+			associateid: '5ec63793345c844e2415d658',
+			slotid: '5ec637a2345c844e2415d65f',
+			slotstarttime: '14:00',
+			custLength: 0,
+			storename: 'Test Store',
+			associatename: 'Associate_3'
+		},
+		slotsAvailable: 1
+	},
+	{
+		_id: {
+			storeid: '5ec54127af24f138c304c6e7',
+			associateid: '5ec5844b17e753712d87eb43',
+			slotid: '5ec584a117e753712d87eb4e',
+			slotstarttime: '15:00',
+			custLength: 0,
+			storename: 'Test Store',
+			associatename: 'Associate_1'
+		},
+		slotsAvailable: 1
+	},
+	{
+		_id: {
+			storeid: '5ec54127af24f138c304c6e7',
+			associateid: '5ec5846e17e753712d87eb46',
+			slotid: '5ec584a817e753712d87eb59',
+			slotstarttime: '15:00',
+			custLength: 0,
+			storename: 'Test Store',
+			associatename: 'Associate_2'
+		},
+		slotsAvailable: 1
+	},
+	{
+		_id: {
+			storeid: '5ec54127af24f138c304c6e7',
+			associateid: '5ec63793345c844e2415d658',
+			slotid: '5ec637a2345c844e2415d660',
+			slotstarttime: '15:00',
+			custLength: 0,
+			storename: 'Test Store',
+			associatename: 'Associate_3'
+		},
+		slotsAvailable: 1
+	},
+	{
+		_id: {
+			storeid: '5ec54127af24f138c304c6e7',
+			associateid: '5ec5844b17e753712d87eb43',
+			slotid: '5ec584a117e753712d87eb4f',
+			slotstarttime: '16:00',
+			custLength: 0,
+			storename: 'Test Store',
+			associatename: 'Associate_1'
+		},
+		slotsAvailable: 1
+	},
+	{
+		_id: {
+			storeid: '5ec54127af24f138c304c6e7',
+			associateid: '5ec5846e17e753712d87eb46',
+			slotid: '5ec584a817e753712d87eb5a',
+			slotstarttime: '16:00',
+			custLength: 0,
+			storename: 'Test Store',
+			associatename: 'Associate_2'
+		},
+		slotsAvailable: 1
+	},
+	{
+		_id: {
+			storeid: '5ec54127af24f138c304c6e7',
+			associateid: '5ec63793345c844e2415d658',
+			slotid: '5ec637a2345c844e2415d661',
+			slotstarttime: '16:00',
+			custLength: 0,
+			storename: 'Test Store',
+			associatename: 'Associate_3'
+		},
+		slotsAvailable: 1
+	},
+	{
+		_id: {
+			storeid: '5ec54127af24f138c304c6e7',
+			associateid: '5ec5844b17e753712d87eb43',
+			slotid: '5ec584a117e753712d87eb50',
+			slotstarttime: '17:00',
+			custLength: 0,
+			storename: 'Test Store',
+			associatename: 'Associate_1'
+		},
+		slotsAvailable: 1
+	},
+	{
+		_id: {
+			storeid: '5ec54127af24f138c304c6e7',
+			associateid: '5ec5846e17e753712d87eb46',
+			slotid: '5ec584a817e753712d87eb5b',
+			slotstarttime: '17:00',
+			custLength: 0,
+			storename: 'Test Store',
+			associatename: 'Associate_2'
+		},
+		slotsAvailable: 1
+	},
+	{
+		_id: {
+			storeid: '5ec54127af24f138c304c6e7',
+			associateid: '5ec63793345c844e2415d658',
+			slotid: '5ec637a2345c844e2415d662',
+			slotstarttime: '17:00',
+			custLength: 0,
+			storename: 'Test Store',
+			associatename: 'Associate_3'
+		},
+		slotsAvailable: 1
+	},
+	{
+		_id: {
+			storeid: '5ec54127af24f138c304c6e7',
+			associateid: '5ec5844b17e753712d87eb43',
+			slotid: '5ec584a117e753712d87eb51',
+			slotstarttime: '18:00',
+			custLength: 0,
+			storename: 'Test Store',
+			associatename: 'Associate_1'
+		},
+		slotsAvailable: 1
+	},
+	{
+		_id: {
+			storeid: '5ec54127af24f138c304c6e7',
+			associateid: '5ec5846e17e753712d87eb46',
+			slotid: '5ec584a817e753712d87eb5c',
+			slotstarttime: '18:00',
+			custLength: 0,
+			storename: 'Test Store',
+			associatename: 'Associate_2'
+		},
+		slotsAvailable: 1
+	},
+	{
+		_id: {
+			storeid: '5ec54127af24f138c304c6e7',
+			associateid: '5ec63793345c844e2415d658',
+			slotid: '5ec637a2345c844e2415d663',
+			slotstarttime: '18:00',
+			custLength: 0,
+			storename: 'Test Store',
+			associatename: 'Associate_3'
+		},
+		slotsAvailable: 1
+	},
+	{
+		_id: {
+			storeid: '5ec54127af24f138c304c6e7',
+			associateid: '5ec63793345c844e2415d658',
+			slotid: '5ec637a2345c844e2415d664',
+			slotstarttime: '19:00',
+			custLength: 0,
+			storename: 'Test Store',
+			associatename: 'Associate_3'
+		},
+		slotsAvailable: 1
+	},
+	{
+		_id: {
+			storeid: '5ec54127af24f138c304c6e7',
+			associateid: '5ec5844b17e753712d87eb43',
+			slotid: '5ec584a117e753712d87eb52',
+			slotstarttime: '19:00',
+			custLength: 0,
+			storename: 'Test Store',
+			associatename: 'Associate_1'
+		},
+		slotsAvailable: 1
+	},
+	{
+		_id: {
+			storeid: '5ec54127af24f138c304c6e7',
+			associateid: '5ec5846e17e753712d87eb46',
+			slotid: '5ec584a817e753712d87eb5d',
+			slotstarttime: '19:00',
+			custLength: 0,
+			storename: 'Test Store',
+			associatename: 'Associate_2'
+		},
+		slotsAvailable: 1
+	}
+];
